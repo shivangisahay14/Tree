2e1be02bb443729d2be18b7776254d53a5a7a286
shivangisahay14 <57834633+shivangisahay14@users.noreply.github.com>
2021-07-07 02:31:05 +0530

    Create 199. Binary Tree Right Side View.cpp

---
+++ 199. Binary Tree Right Side View.cpp
@@ -0,0 +1,27 @@
+ vector<int> rightSideView(TreeNode* root) {
+        
+            vector<int>ans;
+            queue<TreeNode*>q;
+
+        if(root==NULL) return ans;
+
+        q.push(root);
+        while(!q.empty()){
+            int n=q.size();
+            ans.push_back(q.back()->val);
+            while(n--){
+                TreeNode *t=q.front();
+                q.pop();
+
+                if(t->left) {
+                    q.push(t->left);
+                }
+                if(t->right){
+                    q.push(t->right);
+                }
+            }
+
+        }
+            return ans;
+        
+    }
