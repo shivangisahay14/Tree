5300fb1931d10f89c6a9079c796d8aca546ba377
shivangisahay14 <57834633+shivangisahay14@users.noreply.github.com>
2021-07-13 20:02:16 +0530

    Create 124. Binary Tree Maximum Path Sum from any node to any node.cpp

---
+++ 124. Binary Tree Maximum Path Sum from any node to any node.cpp
@@ -0,0 +1,17 @@
+int solve(TreeNode *root,int &res){
+        if(!root) return 0;
+        
+        int l=solve(root->left,res);
+        int r=solve(root->right,res);
+        
+        int temp=max(max(l,r)+root->val,root->val);
+        int ans=max(temp,l+r+root->val);
+        res=max(ans,res);
+        
+        return temp;
+    }
+    int maxPathSum(TreeNode* root) {        
+        int res=INT_MIN;
+        solve(root,res);
+        return res;        
+    }
